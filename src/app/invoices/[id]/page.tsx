52ec6d3d36e9bdf6deb52271d892c31c75110dfd
Miłosz Zawolik <milosz.zawolik@gmail.com>
2026-02-16 11:20:48 +0100

    Complete project implementation

--- src/app/invoices/[id]/page.tsx
+++ src/app/invoices/[id]/page.tsx
@@ -18,7 +18,7 @@ export default function InvoiceDetailsPage() {
   const router = useRouter();
   
   const id = Array.isArray(params.id) ? params.id[0] : params.id;
-  const invoice = getInvoice(id);
+  const invoice = id ? getInvoice(id) : undefined;
 
   if (!invoice) {
       return (
