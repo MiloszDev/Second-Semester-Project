a78c7e1d0202c25e831739516a7afedc56bb47f9
Miłosz Zawolik <milosz.zawolik@gmail.com>
2026-02-16 11:46:59 +0100

    chore: enable standalone build and rename package

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   serverExternalPackages: ["@react-pdf/renderer"],
+  output: "standalone",
 };
 
 export default nextConfig;
